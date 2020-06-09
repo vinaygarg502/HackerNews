67fb2ffd1de70e5945b6ffa375d0fc35b3d78a96
Vinay Garg <vinay.garg@publicissapient.com>
2020-06-09 17:57:56 +0530

    EDITING server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 // we'll talk about this in a minute:
 import serverRenderer from './middleware/renderer';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 // initialize the application and create the routes
